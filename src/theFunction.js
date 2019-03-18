a373f01b2fea71e4c4258632c6ef91725061ec03
Albus Song <strawberry111111@163.com>
2019-03-18 19:57:03 +0800

    can extract data from docx file

--- src/theFunction.js
+++ src/theFunction.js
@@ -1,22 +1,37 @@
-// var admZip = require('adm-zip');
-// const zip = new admZip('test.docx');
 const fs = require("fs");
+var admZip = require('adm-zip');
 
-// document.querySelector('#btnLoad').addEventListener('click', () => {
-//     readText();
-// })
-
-function readText(){
+function readText() {
     const content = fs.readFileSync(__dirname + "/a.txt", "utf8");
-    console.log("the Content: " + content);
-    // const content = fs.readFile(__dirname + "/a.txt", "utf8", (err, data) =>  {
-    //     console.log(err);
-    //     console.log("the Content: " + data);
-    // });    
+    console.log("the Content: " + content);  
 }
 
-function extractDataFromWordFile(){
+function extractDataFromWordFile() {
     console.log('aaaaaaaaadsdddd');
+    const zip = new admZip(__dirname + '/test.docx');
+    let contentXml = zip.readAsText("word/document.xml");
+    let str = "";
+    let tmpStr = "";
+    const filePath = __dirname + "/2.txt";
+    let arrOfItem = contentXml.match(/<w:t>[\s\S]*?<\/w:t>/ig);
+    console.log("arrOfItem: \n" + arrOfItem);
+    for (let i = 0; i < arrOfItem.length; i++) {
+        let item = arrOfItem[i];
+        console.log("\n" + item)
+        tmpStr = item.slice(5,-6);
+        console.log("\n tmpStr: " + tmpStr);
+        str = str + tmpStr + "\n";   
+    }
+    console.log("finalStr: " + str);
+    fs.writeFile(filePath, str, (error) => {
+        if(error) {
+            throw error; 
+        }
+    });
+    // fs.writeFile(filePath, str, (err) => {
+    //     //将./2.txt替换为你要输出的文件路径
+    //     if(err) throw err; 
+    // }
 }
 
 // function readText(file){
